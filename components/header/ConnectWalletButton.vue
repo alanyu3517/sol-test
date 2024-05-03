395b4f04ce98522becb365fe36826a14b70f88b3
Alan Yu <alan3517@protonmail.com>
2024-05-03 18:48:56 +0800

    change new wallet adapter

--- components/header/ConnectWalletButton.vue
+++ components/header/ConnectWalletButton.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <v-btn color="primary text-white" variant="flat" prepend-icon="mdi-wallet">
+    <ClientOnly>
+      <WalletMultiButton />
+    </ClientOnly>
+    <!-- <v-btn color="primary text-white" variant="flat" prepend-icon="mdi-wallet">
       <template v-slot:prepend>
         <v-icon color="white"></v-icon>
       </template>
       Connect Wallet
-    </v-btn>
+    </v-btn> -->
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {WalletMultiButton} from 'solana-wallets-vue';
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,7 @@ import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
 export default defineNuxtConfig({
   //...
   build: {
-    transpile: [
-      'vuetify',
-      '@solana/web3.js',
-      '@solana/wallet-adapter-base'
-    ],
+    transpile: ['vuetify', '@solana/web3.js', '@solana/wallet-adapter-base'],
   },
   css: ['@/assets/scss/style.scss'],
   modules: [
@@ -20,28 +16,30 @@ export default defineNuxtConfig({
     //...
   ],
   vite: {
+    esbuild: {
+      target: 'esnext',
+    },
+    build: {
+      target: 'esnext',
+    },
     vue: {
       template: {
         transformAssetUrls,
       },
     },
     optimizeDeps: {
-      include: [
-        '@solana/web3.js',
-        '@solana/wallet-adapter-base',
-      ]
-    }
+      include: ['@solana/web3.js', '@solana/wallet-adapter-base', '@project-serum/anchor', 'buffer'],
+      esbuildOptions: {
+        target: 'esnext',
+      },
+    },
+    define: {
+      "process.env.BROWSER": true,
+    },
   },
   nitro: {
     rollupConfig: {
-      external: [
-        'borsh',
-        'util',
-        'secp256k1',
-        '@solana/web3.js',
-        '@solana/wallet-adapter-phantom',
-        '@solana/wallet-adapter-base'
-      ],
-    }
+      external: ['borsh', 'util', 'secp256k1', '@solana/web3.js', '@solana/wallet-adapter-phantom', '@solana/wallet-adapter-base'],
+    },
   },
 });
